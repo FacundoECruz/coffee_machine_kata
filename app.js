8b3045abefb8f55ea0b4be468ab1f92c6c295f66
FacundoECruz <facundo.emanuel.cruz.94@gmail.com>
2023-03-16 15:55:54 -0300

    solved second iteration

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
-function translateOrder({ drink, sugar, stick }) {
-  const checkDrink = drink === 'tea' 
-  ? 'T' : (drink === 'chocolate') 
-  ? 'H' : (drink === 'coffee')
-  ? 'C' : null
+function translateOrder({ drink, sugar, stick, money }) {
+
+  function calculateMoney(drink, money) {
+    switch (drink) {
+      case 'tea': {
+        if (money >= 4) {
+          return true
+        }
+        return `${4 - money} euros missing`
+      }
+      case 'chocolate': {
+        if (money >= 5) {
+          return true
+        }
+        return `${5 - money} euros missing`
+      }
+      case 'coffee': {
+        if (money >= 6) {
+          return true
+        }
+        return `${6 - money} euros missing`
+      }
+      default: 'No drink received'
+    }
+  }
+
+  const checkDrink =
+    drink === "tea"
+      ? "T"
+      : drink === "chocolate"
+      ? "H"
+      : drink === "coffee"
+      ? "C"
+      : null;
   const checkSugar = sugar === 0 ? "" : sugar.toString();
   const checkStick = stick ? "0" : "";
+  const checkMoney = calculateMoney(drink, money)
 
   const output = `${checkDrink}:${checkSugar}:${checkStick}`;
-
-  return output;
+  
+  if(typeof checkMoney === 'string'){
+    return checkMoney
+  } else {
+    return output
+  }
 }
 
 export default translateOrder;
--- app.test.js
+++ app.test.js
@@ -5,10 +5,11 @@ test('returns T:1:0 when 1 tea with 1 sugar and a stick', () => {
     drink: 'tea',
     sugar: 1,
     stick: true,
+    money: 4,
   }
-
+  
   const output = 'T:1:0'
-
+  
   expect(translateOrder(input)).toBe(output);
 });
 
@@ -17,10 +18,11 @@ test('returns H:: when 1 chocolate with no sugar and no stick', () => {
     drink: 'chocolate',
     sugar: 0,
     stick: false,
+    money: 5,
   }
-
+  
   const output = 'H::'
-
+  
   expect(translateOrder(input)).toBe(output);
 })
 
@@ -29,9 +31,23 @@ test('returns C:2:0 when 1 coffee with 2 sugar and a stick', () => {
     drink: 'coffee',
     sugar: 2,
     stick: true,
+    money: 6,
+  }
+  
+  const output = 'C:2:0'
+  
+  expect(translateOrder(input)).toBe(output);
+})
+
+test('returns error msg if the amount of money is not enough', () => {
+  const input = {
+    drink: 'coffee',
+    sugar: 2,
+    stick: true,
+    money: 4,
   }
 
-  const output = 'C:2:0'
+  const output = '2 euros missing'
 
-  expect(translateOrder(input)).toBe(output);
+  expect(translateOrder(input)).toBe(output)
 })
